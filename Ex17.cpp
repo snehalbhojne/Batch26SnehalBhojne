f285a31141cd2d00f98ee0a5a142bc0ba89b2e7f
snehalbhojne <93115258+snehalbhojne@users.noreply.github.com>
2021-12-23 15:01:03 +0530

    Add files via upload

---
+++ Ex17.cpp
@@ -0,0 +1,35 @@
+/*WAP to create a class branch with data members branch ID, branch Name,
+ Branch City and member functions as getdata() and Display()*/
+ #include<iostream>
+ #include<string.h>
+ using namespace std;
+ class Branch
+ {
+     public:
+     int branchId;
+     char branchName[30];
+     char branchCity[30];
+   
+   void getdata(int branchId,char branchName[],char branchCity[])
+   {
+       this->branchId=branchId;
+       strcpy(this->branchName,branchName);
+       strcpy(this->branchCity,branchCity);
+   }
+   void Display()
+   {
+       cout<<"\n"<<this->branchId<<"\t"<<this->branchName<<"\t"<<this->branchCity;
+   }
+ };
+ int main()
+ {
+     Branch b1,b2,b3;
+     b1.getdata(103,"Vishrambag","Sangli");
+     b2.getdata(405,"Shivaji Nagar","Pune");
+     b3.getdata(709,"Andheri","Mumbai");
+
+     b1.Display();
+     b2.Display();
+     b3.Display();
+     return 0;
+ }
